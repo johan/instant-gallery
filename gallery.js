4f22428e55e6c80af2bd8ce5a272f51e531a97d2
Johan Sundström <oyasumi@gmail.com>
2010-03-21 20:06:09 -0700

    Sort of silly [C]entering function for use in [F]ull mode, with adjoining keys x/v 1/4 up/down and b scrolling to the bottom. This probably isn't the right UI for shifting the viewport, but it was easy to implement. :-)

--- gallery.js
+++ gallery.js
@@ -65,6 +65,18 @@ function cmd_zoom() {
   show();
 }
 
+function cmd_center(frac_x, frac_y) { try {
+  var img = show.img || {};
+  var ih = img.height, h = innerHeight;
+  var iw = img.width,  w = innerWidth;
+  if (iw && ih) {
+    var y = (ih - h) * frac_y;
+    var x = (iw - w) * frac_x;
+    scrollTo(x, y);
+  }
+  } catch(e) { alert(e.message); }
+}
+
 function cmd_kill(code) {
   urls[at] = undefined;
   if (code == 8)
@@ -129,6 +141,10 @@ var commands = {
   ' ': 'next', 39: 'next',   // arrow right
                27: 'toggle', // escape
     8: "kill", 46: 'kill',   // backspace / delete
+  'X': ['center', 1/2, 1/4],
+  'C': ['center', 1/2, 2/4],
+  'V': ['center', 1/2, 3/4],
+  'B': ['center', 1/2, 4/4],
   'F': 'full',
   'Z': 'zoom',
   'N': 'zoom'
@@ -143,7 +159,9 @@ function key_handler(e) {
   var cmd = commands[code] || commands[key];
   if (cmd) {
     e.preventDefault();
-    self['cmd_'+ cmd](code, key);
-  } // else prompt(String.fromCharCode(e.which), e.keyCode);
+    cmd = 'string' == typeof cmd ? [cmd] : cmd.concat();
+    var fn = self['cmd_'+ cmd.shift()];
+    fn.apply(self, cmd.concat(code, key));
+  }
   return !cmd;
 }
